2d28e5b947c9652327beba5ea1c173c955eb592b
Buslaeva2016 <buslaeva_irinka96@mail.ru>
2016-04-19 10:55:03 +0600

    refactoring

--- calculator.neevin/calculator.neevin/Form1.Designer.cs
+++ calculator.neevin/calculator.neevin/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Caclculate);
             // 
             // button2
             // 
@@ -55,7 +55,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Caclculate);
             // 
             // button3
             // 
@@ -65,7 +65,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.Caclculate);
             // 
             // button4
             // 
@@ -75,7 +75,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.Caclculate);
             // 
             // textBox1
             // 
--- calculator.neevin/calculator.neevin/Form1.cs
+++ calculator.neevin/calculator.neevin/Form1.cs
@@ -17,36 +17,31 @@ namespace Calculator.Neevin
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Caclculate(object sender, EventArgs e)
         {
             double firstValue = Convert.ToDouble(textBox1.Text);
             double secondValue = Convert.ToDouble(textBox2.Text);
-            double result = firstValue + secondValue;
-            label1.Text = result.ToString();
-        }
-        private void button2_Click(object sender, EventArgs e)
-        {
-            double firstValue = Convert.ToDouble(textBox1.Text);
-            double secondValue = Convert.ToDouble(textBox2.Text);
-            double result = firstValue - secondValue;
+            double result;
+            switch (((Button) sender).Name)
+            {
+                case "button1":
+
+                    result = firstValue * secondValue;
+                    break;
+                case "button2":
+                    result = firstValue / secondValue;
+                    break;
+                case "button3":
+                    result = firstValue + secondValue;
+                    break;
+                case "button4":
+                    result = firstValue - secondValue;
+                    break;
+                default:
+                    throw new Exception("Неизвестная операция ");
+            }
             label1.Text = result.ToString();
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-            double firstValue = Convert.ToDouble(textBox1.Text);
-            double secondValue = Convert.ToDouble(textBox2.Text);
-            double result = firstValue / secondValue;
-            label1.Text = result.ToString();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            double firstValue = Convert.ToDouble(textBox1.Text);
-            double secondValue = Convert.ToDouble(textBox2.Text);
-            double result = firstValue * secondValue;
-            label1.Text = result.ToString();
-        }
     }
 }
