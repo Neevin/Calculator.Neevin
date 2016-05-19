d1038b27ae230601e257c38ee714abe7e9a39e9b
1 <1@mail.ru>
2016-05-19 14:40:51 +0600

    XmlComment

--- Calculator.Neevin/Calculator.Neevin/OneArgument/Log.cs
+++ Calculator.Neevin/Calculator.Neevin/OneArgument/Log.cs
@@ -2,8 +2,16 @@
 
 namespace Calculator.Neevin.OneArgument
 {
+    /// <summary>
+    /// Класс для логарифма
+    /// </summary>
     public class Log : ISingleInterface
     {
+        /// <summary>
+        /// Вычисление для логарифма
+        /// </summary>
+        /// <param name="first"></param>
+        /// <returns></returns>
         public double Calculate(double first)
         {
             if (first == -10)
--- Calculator.Neevin/Calculator.Neevin/TwoArgument/Division.cs
+++ Calculator.Neevin/Calculator.Neevin/TwoArgument/Division.cs
@@ -2,8 +2,17 @@
 
 namespace Calculator.Neevin.TwoArgument
 {
+    /// <summary>
+    /// Класс для деления
+    /// </summary>
     public class Division : ICalculate
     {
+        /// <summary>
+        /// Вычисление деления
+        /// </summary>
+        /// <param name="first">первый аргумент</param>
+        /// <param name="second">второй аргумент</param>
+        /// <returns></returns>
         public double Calculate(double first, double second)
         {
             if (second == 0)
