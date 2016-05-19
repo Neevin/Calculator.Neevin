8983ec476eb17e388858910a53e0a4e192f06c94
Neevin <neftyanik1996@mail.ru>
2016-05-19 13:28:49 +0600

    Revert "Revert "New Button Trigonom""
    This reverts commit 2cd46a45bf071ca670bb50971c6da6be1144d2ef.

---
+++ Calculator.Neevin/Calculator.Neevin/Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator.Neevin
+{
+    public class Log : ISingleInterface
+    {
+        public double Calculate(double first)
+        {
+            return Math.Log(first);
+        }
+         
+    }
+}
---
+++ Calculator.Neevin/Calculator.Neevin/Sqrt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator.Neevin
+{
+    public class Sqrt : ISingleInterface
+    {
+        public double Calculate(double first)
+        {
+            return Math.Sqrt(first);
+        }
+         
+    }
+}
